246b99a7e51019dd67438223c1ed13bf40c33487
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-05 11:06:52 -0400

    styling

---
+++ src/components/zip.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './zip.css';
+
+class Zip extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+
+            ]
+        }
+    }
+}
