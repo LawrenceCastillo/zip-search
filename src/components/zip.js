4c8f3fe7e40503e602e615e31ed7ada1c8219b3d
Shu <shu.liang89@gmail.com>
2019-06-05 11:13:39 -0400

    instructions comment identified

--- src/components/zip.js
+++ src/components/zip.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './zip.css';
 
+// Zip should store all the results from the search
 class Zip extends Component {
 
     constructor(props){
         super(props);
         this.state = {
+            // this should store all the data fetched from the API
             data: [
 
             ]
         }
+
+        // functions to bind
+            // 1) function pass into the button  to inititate everything
+            // 2) function to create element for each results and append to result container
+            // renderDefaultView to display nothing when there is no search
+            // renderSearchView to display something when there is a search
+            // optional - reset the search status to false when the user no longer wants to search anything 
     }
+
+
 }
